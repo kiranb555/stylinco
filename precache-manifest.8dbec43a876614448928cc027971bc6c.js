b3e3cd4327b02602a8fbb15e2ea5606067367c0b
kiranb555 <kiran.basavaraj05@gmail.com>
2019-05-31 00:47:30 +0530

    Updates

---
+++ precache-manifest.8dbec43a876614448928cc027971bc6c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5111afcabd79cebae3c72ae32ac7c7",
+    "url": "/stylinco/index.html"
+  },
+  {
+    "revision": "4974976df61460b8d892",
+    "url": "/stylinco/static/css/main.089a2724.chunk.css"
+  },
+  {
+    "revision": "a2722e2b09ce4a7f3df5",
+    "url": "/stylinco/static/js/2.935a9550.chunk.js"
+  },
+  {
+    "revision": "4974976df61460b8d892",
+    "url": "/stylinco/static/js/main.ca7f9a23.chunk.js"
+  },
+  {
+    "revision": "6cc6811f31f14fddf8e2",
+    "url": "/stylinco/static/js/runtime~main.ccd830ae.js"
+  },
+  {
+    "revision": "7b00f4a1081d998ecd2c7fdf201c713c",
+    "url": "/stylinco/static/media/bg-attorney-ppc.7b00f4a1.jpg"
+  },
+  {
+    "revision": "7442efcd315d7045f9e54a3c3119bce8",
+    "url": "/stylinco/static/media/bg-banner-ppc.7442efcd.jpg"
+  },
+  {
+    "revision": "27999e971eda97f41cee376794d68d82",
+    "url": "/stylinco/static/media/body-background-ppc.27999e97.jpg"
+  },
+  {
+    "revision": "0955c2b964cf348c1bddbba4c70d3521",
+    "url": "/stylinco/static/media/brand-ppc.0955c2b9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc84e2ed13fa63aaca2d3e33a6d2fbc5.js"
+  "/stylinco/precache-manifest.8dbec43a876614448928cc027971bc6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stylinco/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
