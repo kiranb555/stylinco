cab7fd45f234cc3d156b80b7f0c61b6779424f15
kiranb555 <kiran.basavaraj05@gmail.com>
2019-05-31 00:33:10 +0530

    Updates

---
+++ precache-manifest.dc84e2ed13fa63aaca2d3e33a6d2fbc5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96979bdcc4cd962720a93a03a673842b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e20833aff97f10636b13",
+    "url": "/static/css/main.3d8a12ce.chunk.css"
+  },
+  {
+    "revision": "a2722e2b09ce4a7f3df5",
+    "url": "/static/js/2.935a9550.chunk.js"
+  },
+  {
+    "revision": "e20833aff97f10636b13",
+    "url": "/static/js/main.d24f0ac0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7b00f4a1081d998ecd2c7fdf201c713c",
+    "url": "/static/media/bg-attorney-ppc.7b00f4a1.jpg"
+  },
+  {
+    "revision": "7442efcd315d7045f9e54a3c3119bce8",
+    "url": "/static/media/bg-banner-ppc.7442efcd.jpg"
+  },
+  {
+    "revision": "27999e971eda97f41cee376794d68d82",
+    "url": "/static/media/body-background-ppc.27999e97.jpg"
+  },
+  {
+    "revision": "0955c2b964cf348c1bddbba4c70d3521",
+    "url": "/static/media/brand-ppc.0955c2b9.png"
+  }
+]);
